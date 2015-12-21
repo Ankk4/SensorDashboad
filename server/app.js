dd75d532a6cee1ee8f64e3e15569cf70eea5ca73
Ankk4 <antero.juutinen@gmail.com>
2015-12-21 20:11:41 +0000

    more cleaning

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+var sp = require("serialport");
 var SerialPort = require("serialport").SerialPort;
 var serialPort = new SerialPort("/dev/ttyACM0", {
   baudrate: 9600
@@ -7,5 +8,13 @@ serialPort.on("open", function () {
   console.log('open');
   serialPort.on('data', function(data) {
     console.log('data received: ' + data);
+  });
+});
+
+sp.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log("Com name: " + port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
   });
 });
