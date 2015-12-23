001c11f1162417641598a4261c6a2c8e85eb6a3f
Ankk4 <antero.juutinen@gmail.com>
2015-12-23 13:20:16 +0200

    Angular Training

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,16 @@
-(function(){
+(function() {
 	var app = angular.module('dashboard', []);
-
 	app.controller('DashboardController', function(){
-
+		// this.property = someobject
 	});
+
+	var senors = [
+		{
+			name: "Tempeture",
+			voltage: 0,
+			celsius: 0,
+			farenheit: 0
+		}
+	];
+
 })();
