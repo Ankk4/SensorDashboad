3583f62ab6be02807633914b7d9a687add33e7c7
Ankk4 <antero.juutinen@gmail.com>
2015-12-26 22:29:33 +0000

    basic stuff done

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,21 @@
 (function(){
 	var app = angular.module('dashboard', []);
-
-	app.controller('DashboardController', function(){
-
+	app.controller('DashboardController', function($scope){
+		
+		$scope.sensors = sens;
 	});
+	
+	var sens = [
+		{
+			name: "Tempeture",
+			Celsius: 0,	
+			Farenheit: 0,
+			VoltageRead: 0,
+			dashboard: true
+		},{
+			name: "Light level",
+			dashboard: false			
+		}
+	];	
 })();
+
