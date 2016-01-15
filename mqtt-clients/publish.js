2deea27d3b5c1f2e4e7d62d46be3c0d42539fc16
Ankk4 <Ankk4>
2016-01-15 14:09:14 +0000

    test clients

--- mqtt-clients/publish.js
+++ mqtt-clients/publish.js
@@ -1,12 +1,15 @@
 // Require the MQTT module as dependency
 // Connect to the broker
-var mqtt 	= require('mqtt'),
+var mqtt 	= require('mqtt');
 var client  = mqtt.connect('mqtt://192.168.202.79', { clientId: 'testPublish-', clean: false });
 
 client.on('connect', function () {
 	console.log("Connected to broker");
 });
 
-client.publish('testPublish', 'This is a test publish', { qos: 1 }, function () {
+// Publish data to topic 'test/mqtt', with Quality of service 1
+// After this, log success to console and close the client
+client.publish('test/mqtt', 'This is a test publish', { qos: 1 }, function () {
 	console.log("Data published");
+	client.end();
 });
--- mqtt-clients/subscribe.js
+++ mqtt-clients/subscribe.js
@@ -2,12 +2,16 @@ var mqtt = require('mqtt');
 
 // MQTT
 var client  = mqtt.connect('mqtt://192.168.202.79', { clientId: 'testSubscription-', clean: false });
+
+//Event listener confirming connection, and then subscribing the client. 
 client.on('connect', function () {
 	console.log("Connected to broker");
-	client.subscribe('#', { qos: 1 });
+	client.subscribe('test/mqtt', { qos: 1 });
 });
+
+// Event listener looking for messages send from subscriptions
+// 'message' is a buffer type
 client.on('message', function (topic, message) { 
-	//sensorData = JSON.parse(message);
-	console.log("Message recived: ", message);
+	console.log("Message recived: ", message.toString());
 });
 
