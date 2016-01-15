34200f1d89c37316dbcbb8a9ff52d78a78db2f01
Antero Juutinen <antero.juutinen@gmail.com>
2016-01-15 20:26:04 +0200

    eh

--- mqtt-clients/publish.js
+++ mqtt-clients/publish.js
@@ -1,15 +1,22 @@
 // Require the MQTT module as dependency
-// Connect to the broker
+// Connect to the broker - clientId is connected to a queue
 var mqtt 	= require('mqtt');
-var client  = mqtt.connect('mqtt://192.168.202.79', { clientId: 'testPublish-', clean: false });
+var client  = mqtt.connect('mqtt://192.168.0.15:1883', { 
+	clientId: 'testClient2-', 
+	clean: false
+});
 
-client.on('connect', function () {
-	console.log("Connected to broker");
+client.on('connect', function () {  
+	console.log('Connected')
 });
 
 // Publish data to topic 'test/mqtt', with Quality of service 1
 // After this, log success to console and close the client
-client.publish('test/mqtt', 'This is a test publish', { qos: 1 }, function () {
-	console.log("Data published");
-	client.end();
-});
+// Do it every second
+setInterval(function(){
+	client.publish('test/mqtt', 'This is from client 2.', { qos: 1 }, function() {
+		console.log('Message published from client 2.')
+	});
+}, 1000);
+
+
--- mqtt-clients/subscribe.js
+++ mqtt-clients/subscribe.js
@@ -1,17 +1,19 @@
 var mqtt = require('mqtt');
 
-// MQTT
-var client  = mqtt.connect('mqtt://192.168.202.79', { clientId: 'testSubscription-', clean: false });
+var client  = mqtt.connect('mqtt://192.168.0.15',  { 
+	clientId: 'testClient3-', 
+	clean: false 
+});
 
 //Event listener confirming connection, and then subscribing the client. 
 client.on('connect', function () {
-	console.log("Connected to broker");
+	console.log('Connected to broker');
 	client.subscribe('test/mqtt', { qos: 1 });
 });
 
 // Event listener looking for messages send from subscriptions
 // 'message' is a buffer type
 client.on('message', function (topic, message) { 
-	console.log("Message recived: ", message.toString());
+	console.log('Message received: ', message.toString());
 });
 
